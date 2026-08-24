e648ff3b76f59371f81916587330bd3ad9bf42d1
PedroTMontezano <montezano.pedro90@gmail.com>
2026-08-24 19:55:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const formulario = document.getElementById("form-tarefa");
+const campoTarefa = document.getElementById("tarefa");
+const listaTarefas = document.getElementById("lista-tarefas");
 
+formulario.addEventListener("submit", function(evento) {
+    evento.preventDefault();
+
+    const textoTarefa = campoTarefa.value;
+
+    const novaTarefa = document.createElement("li");
+    novaTarefa.textContent = textoTarefa;
+
+    listaTarefas.appendChild(novaTarefa);
+
+    campoTarefa.value = "";
+});
